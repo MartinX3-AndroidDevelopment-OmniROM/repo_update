4cf3357cc41f61f0c7bf385bc6e8aa2e840706be
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2020-10-27 18:55:06 +0100

    omnify

--- repo_update.sh
+++ repo_update.sh
@@ -65,60 +65,60 @@ enter_aosp_dir vendor/qcom/opensource/data/ipacfg-mgr/sdm845 hardware/qcom/sdm84
 apply_gerrit_cl_commit refs/changes/23/834623/2 0f42902cbc526d6d5badcece2add39d5badd1537
 popd
 
-enter_aosp_dir hardware/qcom/audio
+#enter_aosp_dir hardware/qcom/audio # Does not exist in minimal omni 10
 # hal: Correct mixer control name for 3.5mm headphone
 # Change-Id: I749609aabfed53e8adb3575695c248bf9a674874
-git revert --no-edit 39a2b8a03c0a8a44940ac732f636d9cc1959eff2
+#git revert --no-edit 39a2b8a03c0a8a44940ac732f636d9cc1959eff2
 
 # Switch msmnile to new Audio HAL
 # Change-Id: I28e8c28822b29af68b52eb84f07f1eca746afa6d
-git revert --no-edit d0d5c9135fed70a25a42f09f0e32b056bc7b15a8
+#git revert --no-edit d0d5c9135fed70a25a42f09f0e32b056bc7b15a8
 
 # switch sm8150 to msmnile
 # Change-id: I37b9461240551037812b35d96d0b2db5e30bae5f
-git revert --no-edit 8e9b92d2c87e9d1cd96ef153853287cb79d5934c
+#git revert --no-edit 8e9b92d2c87e9d1cd96ef153853287cb79d5934c
 
 #Add msm8976 tasha sound card detection to msm8916 HAL
 #Change-Id:  Idc5ab339bb9c898205986ba0b4c7cc91febf19de
-apply_gerrit_cl_commit refs/changes/99/1112099/2 5d6e73eca6f83ce5e7375aa1bd6ed61143d30978
+#apply_gerrit_cl_commit refs/changes/99/1112099/2 5d6e73eca6f83ce5e7375aa1bd6ed61143d30978
 
 #hal: enable audio hal on sdm660
 #Change-Id: I7bb807788e457f7ec6ce5124dfb1d88dc96d8127
-apply_gerrit_cl_commit refs/changes/00/1112100/2 eeecf8a399080598e5290d3356b0ad557bd0ccbd
+#apply_gerrit_cl_commit refs/changes/00/1112100/2 eeecf8a399080598e5290d3356b0ad557bd0ccbd
 
 # hal: msm8916: Fix for vndk compilation errors
 # Change-Id: Iffd8a3c00a2a1ad063e10c0ebf3ce9e88e3edea0
-apply_gerrit_cl_commit refs/changes/14/777714/1 065ec9c4857fdd092d689a0526e0caeaaa6b1d72
+#apply_gerrit_cl_commit refs/changes/14/777714/1 065ec9c4857fdd092d689a0526e0caeaaa6b1d72
 
 # hal: msm8916: Add missing bracket to close function definition.
 # Change-Id: I8296a8fb551097fabf72115d2cec0849671b91ea
-apply_gerrit_cl_commit refs/changes/51/1118151/1 b7c1366360089d6cd1b4b18c70085a802a6a0544
-popd
+#apply_gerrit_cl_commit refs/changes/51/1118151/1 b7c1366360089d6cd1b4b18c70085a802a6a0544
+#popd
 
 enter_aosp_dir hardware/qcom/bootctrl
 # Build bootctrl.sdm710 with Android.bp.
 # Change-Id: Ib29d901b44ad0ec079c3e979bfdcd467e1a18377
-apply_gerrit_cl_commit refs/changes/01/965401/1 c665a9c43f379f754b4ee25df2818b6c20c5346e
+#apply_gerrit_cl_commit refs/changes/01/965401/1 c665a9c43f379f754b4ee25df2818b6c20c5346e # Already in TeamWin project
 # Revert^2 "Build bootctrl.msm8998 with Android.bp.""
 # Change-Id: I6a85b7885903df818deb32c40c751ac4358a6dbc
-apply_gerrit_cl_commit refs/changes/93/968693/1 1933d30528c58598d7423d8b307d8e0fd2c50ad5
+#apply_gerrit_cl_commit refs/changes/93/968693/1 1933d30528c58598d7423d8b307d8e0fd2c50ad5 # Already in TeamWin project
 # Build bootctrl.msm8996 with Android.bp.
 # Android.mk itself will be removed in a separate CL.
 # Change-Id: I864bd626d25723bd390b2453022d9cd47a54d2a2
-apply_gerrit_cl_commit refs/changes/96/967996/3 b229dfc102d5ea8e659514c61f6520ab3f9f777c
+#apply_gerrit_cl_commit refs/changes/96/967996/3 b229dfc102d5ea8e659514c61f6520ab3f9f777c # Already in TeamWin project
 # Remove Android.mk rules for building bootctrl.
 # Change-Id: Ib110508065f47a742acd92e03ea42901e8002e4f
-apply_gerrit_cl_commit refs/changes/87/971787/1 7bde6868ff24001f8b6deb8cf643d86d71978b93
+#apply_gerrit_cl_commit refs/changes/87/971787/1 7bde6868ff24001f8b6deb8cf643d86d71978b93 # Already in TeamWin project
 popd
 
 enter_aosp_dir hardware/nxp/nfc
 # hardware: nxp: Restore pn548 support to 1.1 HAL
 # Change-Id: Ifbef5a5ec0928b0a90b2fc71d84872525d0cf1a6
-apply_gerrit_cl_commit refs/changes/77/980177/3 0285b720ea752c8dcf28c35d794990e982103ada
+#apply_gerrit_cl_commit refs/changes/77/980177/3 0285b720ea752c8dcf28c35d794990e982103ada # Already in OmniROM project
 # hardware: nxp: Restore pn547 support
 # Change-Id: I226fa084d22850a8610f1d67ef30b96250fbd570
 # (Cherry-picked from: I498367f676f8c8d7fc13e849509d0d8a05ec89a8)
-apply_gerrit_cl_commit refs/changes/69/980169/2 a58def9e0ce610f1a349d5de31f267129a0a2397
+#apply_gerrit_cl_commit refs/changes/69/980169/2 a58def9e0ce610f1a349d5de31f267129a0a2397 # Already in OmniROM project
 popd
 
 enter_aosp_dir hardware/interfaces
@@ -150,15 +150,15 @@ enter_aosp_dir packages/apps/DeskClock
 # DeskClock - Moved the android:targetSdkVersion to 25 to fix "Clock has stopped"
 # message displayed when Alarm trigger.
 # Change-Id: I75a96e1ed4acebd118c212b51b7d0e57482a66bb
-apply_gerrit_cl_commit refs/changes/26/987326/1 e6351b3b85b2f5d53d43e4797d3346ce22a5fa6f
+#apply_gerrit_cl_commit refs/changes/26/987326/1 e6351b3b85b2f5d53d43e4797d3346ce22a5fa6f # Already in OmniROM project
 popd
 
-enter_aosp_dir packages/apps/Messaging
+#enter_aosp_dir packages/apps/Messaging # Does not exist in minimal omni 10
 # AOSP/Messaging - Update the Messaging version to 24 until notification
 # related logic changes are made.
 # Change-Id: Ic263e2c63d675c40a2cfa1ca0a8776c8e2b510b9
-apply_gerrit_cl_commit refs/changes/82/941082/1 8e71d1b707123e1b48b5529b1661d53762922400
-popd
+#apply_gerrit_cl_commit refs/changes/82/941082/1 8e71d1b707123e1b48b5529b1661d53762922400
+#popd
 
 # because "set -e" is used above, when we get to this point, we know
 # all patches were applied successfully.
